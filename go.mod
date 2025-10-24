6bcc03aeb06515880607e3d3c78200edb27c56c9
vignesh-j-shetty <vigneshshetty838@gmail.com>
2025-10-24 09:57:56 +0530

    Removed postgres

--- go.mod
+++ go.mod
@@ -5,24 +5,18 @@ go 1.24.6
 require (
 	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
+	github.com/shirou/gopsutil/v3 v3.24.5
 	google.golang.org/grpc v1.76.0
 	google.golang.org/protobuf v1.36.9
 )
 
 require (
 	github.com/go-ole/go-ole v1.2.6 // indirect
-	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
-	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
-	github.com/shirou/gopsutil/v3 v3.24.5 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
-	golang.org/x/crypto v0.40.0 // indirect
-	golang.org/x/sync v0.16.0 // indirect
 )
 
 require (
-	github.com/jackc/pgx/v5 v5.7.6
 	golang.org/x/net v0.42.0 // indirect
 	golang.org/x/sys v0.35.0 // indirect
 	golang.org/x/text v0.27.0 // indirect
--- internal/mainnode/mainnodeserviceImpl.go
+++ internal/mainnode/mainnodeserviceImpl.go
@@ -2,45 +2,19 @@ package mainnode
 
 import (
 	"context"
-	"github.com/vignesh-j-shetty/GoDFS/internal/mainnode/config"
-	"github.com/vignesh-j-shetty/GoDFS/internal/mainnode/repository"
 	pb "github.com/vignesh-j-shetty/GoDFS/pkg/api"
 )
 
 type MainNodeServiceImpl struct {
-	repository repository.DataNodeRepository
 	pb.UnimplementedMainNodeServiceServer
 }
 
 func NewMainNodeService() (*MainNodeServiceImpl, error) {
-	config := config.InitConfig()
-	repo, err := repository.NewDataNodeRepositoryPostgres(config)
-	if err != nil {
-		return nil, err
-	}
-	return &MainNodeServiceImpl{
-		repository: repo,
-	}, nil
+	return nil, nil
 }
 
 // Register implements rpc.MetaDataServiceServer.
 func (mds MainNodeServiceImpl) Register(ctx context.Context, dataNodeInfo *pb.DataNodeInfo) (*pb.RegisterReply, error) {
 
-	data := repository.DataNode {
-		NodeId: dataNodeInfo.NodeId,
-		RpcEndpoint: dataNodeInfo.RpcEndpoint,
-		Role: "",
-	}
-
-	err := mds.repository.CreateDataNode(ctx, data)
-	if err != nil {
-		if err == repository.ErrDuplicateDataNode {
-			err = mds.repository.UpdateRpcEndpoint(ctx, data.NodeId, data.RpcEndpoint)
-			if err != nil {
-				return &pb.RegisterReply{Status: "FAILURE", ErrorMsg: err.Error()}, nil
-			}
-		}
-		return nil, err
-	}
-	return &pb.RegisterReply{Status: "SUCCESS"}, nil
+	return nil, nil
 }
