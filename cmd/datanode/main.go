ade9533585b9ca6237f5bca4523090ccbab17293
vignesh-j-shetty <vigneshshetty838@gmail.com>
2025-10-27 09:55:22 +0530

    Removed gRPC Heartbeat service

--- cmd/datanode/main.go
+++ cmd/datanode/main.go
@@ -1,18 +1,11 @@
 package main
 
 import (
-	"context"
 	"github.com/joho/godotenv"
 	"github.com/vignesh-j-shetty/GoDFS/internal/datanode/config"
-	"github.com/vignesh-j-shetty/GoDFS/internal/datanode/service"
 )
 
 func main() {
 	_ = godotenv.Load()
-	config := config.InitConfig()
-	heartHeatbeatService := service.HeartbeatClientService {
-		DataNodeConfig: config,
-	}
-	heartHeatbeatService.StartHeartbeatService(context.Background())
-	heartHeatbeatService.Wait()
+	config.InitConfig()
 }
--- cmd/mainnode/main.go
+++ cmd/mainnode/main.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	"log"
-	"net"
 	"sync"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/vignesh-j-shetty/GoDFS/internal/mainnode/handler"
-	"github.com/vignesh-j-shetty/GoDFS/internal/mainnode/service"
-	pb "github.com/vignesh-j-shetty/GoDFS/pkg/rpc"
-	"google.golang.org/grpc"
 )
 
 func main() {
@@ -21,29 +17,12 @@ func main() {
 	folderOps := handler.NewFolderOpsHandler()
 	folderOps.InitRoutes(r)
 
-	// Grpc setup 
-	grpcAddr := ":50051"
-	lis, err := net.Listen("tcp", grpcAddr)
-	if err != nil {
-		log.Fatalf("failed to listen on %s: %v", grpcAddr, err)
-	}
-	grpcSrv := grpc.NewServer()
-	pb.RegisterHeartBeatServiceServer(grpcSrv, &service.HeartBeatService {})
-
 	var wg sync.WaitGroup
-	wg.Add(2)
+	wg.Add(1)
 	go func() {
 		defer wg.Done()
 		log.Printf("https server listening on 8080")
 		r.Run(":8080")
-	}()
-
-	go func() {
-		defer wg.Done()
-		log.Printf("gRPC server listening on %s", grpcAddr)
-		if err := grpcSrv.Serve(lis); err != nil {
-			log.Printf("Error in starting gRPC server %s\n", err.Error())
-		}
 	}()
 	wg.Wait()
 }
